58f5d1ff55d6e88fdf8de87b24666c00e67c8872
Roberto Paz <rpaz@proofpoint.com>
2022-07-13 10:25:24 -0300

    First commit

---
+++ backend/pages/api/hello.js
@@ -0,0 +1,5 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ hello: 'This is a Hello message from API' })
+}
